df31e861a91497ba946948a043cc48894fe28565
Gabriel <fco.gabrielsilva2408@gmail.com>
2026-01-30 14:43:34 -0300

    Aula 146 - Dismiss Keyboard View

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { PublicRoutes } from "./PublicRoutes";
 import { useCallback, useState } from "react";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { SystemBars } from "react-native-edge-to-edge";
 
 const NavigationRoutes = () => {
   const [user, setUser] = useState(undefined);
@@ -16,6 +17,7 @@ const NavigationRoutes = () => {
 
   return (
     <NavigationContainer>
+      <SystemBars style={"light"} />
       <Routes />
     </NavigationContainer>
   );
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,18 +1,20 @@
+import { DismissKeyboardView } from "@/components/DismissKeyboardView";
 import { PublicStackParamsList } from "@/routes/PublicRoutes";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export const Login = () => {
   const navigation =
     useNavigation<StackNavigationProp<PublicStackParamsList>>();
 
   return (
-    <View className="flex-1 items-center justify-center">
+    <DismissKeyboardView>
       <Text>Tela de login</Text>
+      <TextInput className="bg-gray-500 w-full" />
       <TouchableOpacity onPress={() => navigation.navigate("Register")}>
         <Text>Registrar</Text>
       </TouchableOpacity>
-    </View>
+    </DismissKeyboardView>
   );
 };
